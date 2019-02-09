6fca6a5491349cbf94e72eb16ca8ab751cdac248
Kento75 <kento2github@gmail.com>
2019-02-09 21:09:15 +0900

    Add base app

---
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// いわゆるimport文
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+    public function index(Request $request) {
+        $data = [
+            'msg' => 'これはコントローラから渡されたメッセージです。',
+            'id' => $request -> id
+        ];
+        return view('hello.index', $data);
+    }
+}
