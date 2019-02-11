0545b7c7781b6780259fb711459dd8fcff81b046
Kento75 <kento2github@gmail.com>
2019-02-11 14:59:50 +0900

    Add global middleware

--- laravelapp/app/Http/Kernel.php
+++ laravelapp/app/Http/Kernel.php
@@ -19,6 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        /** My Global Middleware */
+        \App\Http\Middleware\HelloMiddleware::class,
     ];
 
     /**
