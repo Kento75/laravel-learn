1036f3a30c008cd3ec95322f285ad6c2f774c6f0
Kento75 <kento2github@gmail.com>
2019-02-11 14:56:12 +0900

    Add middleware

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -10,13 +10,7 @@ class HelloController extends Controller
 {
     public function index(Request $request) {
 
-        // $data = [
-        //     ['name' => '山田太郎', 'mail' => 'taro@hoge'],
-        //     ['name' => '田中はなこ', 'mail' => 'hanako@hoge'],
-        //     ['name' => '鈴木五郎', 'mail' => 'goro@hoge'],
-        // ];
-
-        return view('hello.index', ['message' => 'Hello!']);
+        return view('hello.index');
     }
 
     public function post(Request $request) {
--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -9,8 +9,8 @@
 
 @section('content')
   <p>ここが本文のコンテンツです</p>
-  <p>Controller Value<br>'message' = {{$message}}</p>
-  <p>ViewComposer Value<br>'view_message' = {{$view_message}}</p>
+  <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
+  <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
 @endsection
 
 @section('footer')
