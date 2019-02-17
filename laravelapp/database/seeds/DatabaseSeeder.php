9c13e53ecc9e26382e5ea16b1a8fe7b67329aace
Kento75 <kento2github@gmail.com>
2019-02-18 06:19:33 +0900

    Add Rest route

--- laravelapp/database/seeds/DatabaseSeeder.php
+++ laravelapp/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this -> call(PeopleTableSeeder::class);
+        // $this -> call(PeopleTableSeeder::class);
+        $this -> call(RestdataTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -47,3 +47,6 @@ Route::post('person/del', 'PersonController@remove');
 Route::get('board', 'BoardController@index');
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+
+// REST Route
+Route::resource('rest', 'RestappController');
